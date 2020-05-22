7672f0dfbdd30ff13bd5010e28b89e68093f75d0
conao3 <conao3@gmail.com>
2020-05-22 10:58:19 +0900

    add dependency functions

--- pile.el
+++ pile.el
@@ -60,12 +60,14 @@ See `magit-process-buffer'."
       (switch-to-buffer-other-window (current-buffer)))
     (current-buffer)))
 
-(defun pile--insert-section (program args)
-  "Insert new section for PROGRAM with ARGS.
+(defun pile--insert-section (command)
+  "Insert new section for COMMAND.
 See `magit-process-insert-section'."
   (let ((inhibit-read-only t)
         (magit-insert-section--parent magit-root-section)
-        (magit-insert-section--oldroot nil))
+        (magit-insert-section--oldroot nil)
+        (program (car command))
+        (args (cdr command)))
     (goto-char (1- (point-max)))
     (prog1 (magit-insert-section (process)
              (insert (propertize (file-name-nondirectory program)
@@ -84,35 +86,140 @@ See `magit-process-insert-section'."
   (set (make-local-variable 'eshell-last-output-end) (point-marker))
   (set (make-local-variable 'eshell-last-output-block-begin) (point)))
 
+(defun pile--promise-make-process-with-handler (command &optional handler merge-stderr)
+  "Return promise to make new asynchronous COMMAND.
+
+Arguments:
+  - PROGRAM is shell commands list of string.
+  - HANDLER is function, called with process object after program is invoked.
+  - MERGE-STDERR is boolean, whether merge stdout and stderr
+
+Resolve:
+  - A list like as (stdout stderr) when process finish with exitcode 0.
+    stdout and stderr are string.
+
+Reject:
+  - A list like as (event stdout stderr) when process doesn't finish exitcode 0.
+    event, stdout and stderr are string.
+    The event is documented at https://www.gnu.org/software/emacs/manual/html_node/elisp/Sentinels.html"
+  (promise-new
+   (lambda (resolve reject)
+     (let* ((program (car command))
+            (stdout (generate-new-buffer (concat "*" program "-stdout*")))
+            (stderr (generate-new-buffer (concat "*" program "-stderr*")))
+            (stderr-pipe (unless merge-stderr
+                           (make-pipe-process
+                            :name (concat "*" program "-stderr-pipe*")
+                            :noquery t
+                            ;; use :filter instead of :buffer, to get rid of "Process Finished" lines
+                            :filter (lambda (_ output)
+                                      (with-current-buffer stderr
+                                        (insert output))))))
+            (cleanup (lambda ()
+                       (kill-buffer stdout)
+                       (unless merge-stderr
+                         (delete-process stderr-pipe)
+                         (kill-buffer stderr)))))
+       (condition-case err
+           (let ((proc (if merge-stderr
+                           (make-process :name program
+                                         :buffer stdout
+                                         :command command)
+                         (make-process :name program
+                                       :buffer stdout
+                                       :command command
+                                       :stderr stderr-pipe))))
+             (set-process-sentinel
+              proc
+              (lambda (_process event)
+                (unwind-protect
+                    (let ((stdout-str (with-current-buffer stdout
+                                        (buffer-string)))
+                          (stderr-str (unless merge-stderr
+                                        (with-current-buffer stderr
+                                          (buffer-string)))))
+                      (if (string= event "finished\n")
+                          (funcall resolve (list stdout-str stderr-str))
+                        (funcall reject (list event stdout-str stderr-str))))
+                  (funcall cleanup))))
+             (when handler
+               (funcall handler proc)))
+         (error (funcall cleanup)
+                (signal (car err) (cdr err))))))))
+
+(defun pile--output-filter (buf string)
+  "Send the output to BUF from PROCESS (STRING) to the interactive display.
+This is done after all necessary filtering has been done."
+  (let ((oprocbuf buf)
+        (inhibit-read-only t)
+	(inhibit-point-motion-hooks t)
+	(inhibit-modification-hooks t))
+    (let ((functions eshell-preoutput-filter-functions))
+      (while (and functions string)
+	(setq string (funcall (car functions) string))
+	(setq functions (cdr functions))))
+    (if (and string oprocbuf (buffer-name oprocbuf))
+	(let (opoint obeg oend)
+	  (with-current-buffer oprocbuf
+	    (setq opoint (point))
+	    (setq obeg (point-min))
+	    (setq oend (point-max))
+	    (let ((buffer-read-only nil)
+		  (nchars (length string))
+		  (ostart nil))
+	      (widen)
+	      (goto-char eshell-last-output-end)
+	      (setq ostart (point))
+	      (if (<= (point) opoint)
+		  (setq opoint (+ opoint nchars)))
+	      (if (< (point) obeg)
+		  (setq obeg (+ obeg nchars)))
+	      (if (<= (point) oend)
+		  (setq oend (+ oend nchars)))
+              ;; Let the ansi-color overlay hooks run.
+              (let ((inhibit-modification-hooks nil))
+                (insert-before-markers string))
+	      (if (= (window-start) (point))
+		  (set-window-start (selected-window)
+				    (- (point) nchars)))
+	      (if (= (point) eshell-last-input-end)
+		  (set-marker eshell-last-input-end
+			      (- eshell-last-input-end nchars)))
+	      (set-marker eshell-last-output-start ostart)
+	      (set-marker eshell-last-output-end (point))
+	      (force-mode-line-update))
+	    (narrow-to-region obeg oend)
+	    (goto-char opoint)
+	    (eshell-run-output-filters))))))
+
 (defvar pile-process nil)
 
-(defun pile--promise-make-process (section program args)
-  "Run PROGRAM with ARGS and output at SECTION."
+(defun pile--promise-make-process (section command)
+  "Run COMMAND and output at SECTION."
   (let* ((ptr (oref section end))
          (buf (marker-buffer ptr)))
     (with-current-buffer buf
       (goto-char (- ptr 1))
       (pile--prepare-eshell-marker)
-      (apply
-       #'promise:make-process-with-handler
-       program
+      (pile--promise-make-process-with-handler
+       command
        (lambda (proc)
          (setq pile-process proc)
          (set-process-filter
           proc
           (lambda (proc string)
-            (let ((inhibit-read-only t))
-              (eshell-output-filter proc string)))))
-       args))))
+            (setq pile-process proc)
+            (pile--output-filter buf string))))
+       'merge))))
 
 
 ;;; main
 
-(async-defun pile-make-process (program &rest args)
-  "Exec PROGRAM with ARGS via `shell-command' async and output `pile-buffer'.
+(async-defun pile-make-process (command)
+  "Exec COMMAND via `shell-command' async and output `pile-buffer'.
 See `magit-call-process'."
-  (let ((section (pile--insert-section program args)))
-    (await (pile--promise-make-process section program args))))
+  (let ((section (pile--insert-section command)))
+    (await (pile--promise-make-process section command))))
 
 (define-derived-mode pile-section-mode magit-section-mode "Pile"
   "Major-mode for pile buffer."
